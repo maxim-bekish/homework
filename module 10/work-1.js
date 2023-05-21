ae41b57f3b656d4fcd34300ac6488c18c6508544
Maksim Bekis <maxamax997@gmail.com>
2023-05-21 12:08:32 +0300

    work 13.0.1

--- module 10/work-1.js
+++ module 10/work-1.js
@@ -1,6 +1,5 @@
 let result = +prompt("Введите четное число");
-
-if (isNaN(result % 2)) {
+ if (isNaN(result % 2)) {
   console.log("не число");
 } else if (result % 2 == 0) {
   console.log("четное");
---
+++ module 13 OOP/js.js
@@ -0,0 +1,12 @@
+////// Задание 13.0.1 (на самопроверку)
+
+// const arr = [{ a: 1, b: 2 }, { a: "value" }, { o: null }, { a: true, m: 123 }];
+// (function () {
+//   arr.forEach(function (item, i, arr) {
+//     if (arr[i].a === undefined) {
+//       item.a = "няма";
+//     }
+//     return console.log(`${item.a} ${i}`);
+//   });
+// })();
+
